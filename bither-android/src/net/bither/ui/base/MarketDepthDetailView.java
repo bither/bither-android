b714d0cb2832cd3a2d56912c94ad7f7c3d500dde
jjz <woaf1003@gmail.com>
2014-05-23 14:09:05 +0800

    display MarketDepthDetail and klineDetailView

--- bither-android/src/net/bither/ui/base/MarketDepthDetailView.java
+++ bither-android/src/net/bither/ui/base/MarketDepthDetailView.java
@@ -20,6 +20,8 @@ import net.bither.R;
 import net.bither.preference.AppSharedPreference;
 import net.bither.util.AnimationUtil;
 import net.bither.util.CurrencySymbolUtil;
+import net.bither.util.LogUtil;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Handler;
@@ -32,97 +34,96 @@ import android.widget.TextView;
 
 public class MarketDepthDetailView extends LinearLayout {
 
-	private TextView tvOrder;
-	private TextView tvPrice;
-	private TextView tvVolume;
-	private TextView tvSymbol;
+    private TextView tvOrder;
+    private TextView tvPrice;
+    private TextView tvVolume;
+    private TextView tvSymbol;
 
-	private ImageView ivSymbolBtc;
-	private Bitmap btcBit;
+    private ImageView ivSymbolBtc;
+    private Bitmap btcBit;
 
-	public MarketDepthDetailView(Context context) {
-		super(context);
-		initView();
-	}
+    public MarketDepthDetailView(Context context) {
+        super(context);
+        initView();
+    }
 
-	public MarketDepthDetailView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		initView();
+    public MarketDepthDetailView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
 
-	}
+    }
 
-	public MarketDepthDetailView(Context context, AttributeSet attrs,
-			int defStyle) {
-		super(context, attrs, defStyle);
-		initView();
-	}
+    public MarketDepthDetailView(Context context, AttributeSet attrs,
+                                 int defStyle) {
+        super(context, attrs, defStyle);
+        initView();
+    }
 
-	private void initView() {
-		removeAllViews();
-		addView(LayoutInflater.from(getContext()).inflate(
-				R.layout.market_depth_detail_view, null),
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		this.tvOrder = (TextView) findViewById(R.id.tv_detail_order);
-		this.tvPrice = (TextView) findViewById(R.id.tv_detail_price);
-		this.tvVolume = (TextView) findViewById(R.id.tv_detail_volume);
-		this.ivSymbolBtc = (ImageView) findViewById(R.id.iv_symbol_btc);
-		this.btcBit = CurrencySymbolUtil.getBtcSlimSymbol(this.tvVolume);
-		tvSymbol = (TextView) findViewById(R.id.tv_symbol);
+    private void initView() {
+        removeAllViews();
+        addView(LayoutInflater.from(getContext()).inflate(
+                        R.layout.market_depth_detail_view, null),
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT
+        );
+        this.tvOrder = (TextView) findViewById(R.id.tv_detail_order);
+        this.tvPrice = (TextView) findViewById(R.id.tv_detail_price);
+        this.tvVolume = (TextView) findViewById(R.id.tv_detail_volume);
+        this.ivSymbolBtc = (ImageView) findViewById(R.id.iv_symbol_btc);
+        this.btcBit = CurrencySymbolUtil.getBtcSlimSymbol(this.tvVolume);
+        tvSymbol = (TextView) findViewById(R.id.tv_symbol);
 
-	}
+    }
 
-	public void setContent(String order, String price, String volume) {
-		this.tvOrder.setText(order);
-		this.tvPrice.setText(price);
-		this.tvVolume.setText(" " + volume);
-		this.ivSymbolBtc.setImageBitmap(btcBit);
-		this.tvSymbol.setText(AppSharedPreference.getInstance()
-				.getDefaultExchangeRate().getSymbol());
-	}
+    public void setContent(String order, String price, String volume) {
 
-	public void hide() {
-		AnimationUtil.fadeIn(MarketDepthDetailView.this);
+        this.tvOrder.setText(order);
+        this.tvPrice.setText(price);
+        this.tvVolume.setText(" " + volume);
+        this.ivSymbolBtc.setImageBitmap(btcBit);
+        this.tvSymbol.setText(AppSharedPreference.getInstance()
+                .getDefaultExchangeRate().getSymbol());
+    }
 
-	}
+    public void hide() {
+        AnimationUtil.fadeIn(MarketDepthDetailView.this);
 
-	public void notifyViewMove(int x, int y, int parentWidth) {
-		clearAnimation();
-		if (getVisibility() != View.VISIBLE) {
-			setVisibility(View.VISIBLE);
-			if (x > parentWidth / 2) {
-				moveViewDelayed(x, y, parentWidth);
-			} else {
-				moveView(x, y, parentWidth);
-			}
+    }
 
-		} else {
-			moveView(x, y, parentWidth);
-		}
-	}
+    public void notifyViewMove(int x, int y, int parentWidth) {
+        clearAnimation();
+        if (getVisibility() != View.VISIBLE) {
+            setVisibility(View.VISIBLE);
+        }
+        if (x > parentWidth / 2) {
+            moveViewDelayed(x, y, parentWidth);
+        } else {
+            moveView(x, y, parentWidth);
+        }
+    }
 
-	private void moveViewDelayed(final int x, final int y, final int parentWidth) {
-		if (getWidth() != 0) {
-			moveView(x, y, parentWidth);
-		} else {
-			new Handler().postDelayed(new Runnable() {
+    private void moveViewDelayed(final int x, final int y, final int parentWidth) {
+        if (getWidth() > 0) {
+            moveView(x, y, parentWidth);
+        } else {
+            new Handler().postDelayed(new Runnable() {
 
-				@Override
-				public void run() {
-					moveViewDelayed(x, y, parentWidth);
-				}
-			}, 30);
-		}
-	}
+                @Override
+                public void run() {
+                    moveViewDelayed(x, y, parentWidth);
+                }
+            }, 100);
+        }
+    }
 
-	private void moveView(int x, int y, int parentWidth) {
-		int leftMargin = 0;
-		if (x > parentWidth / 2) {
-			leftMargin = x - getWidth();
-		} else {
-			leftMargin = x;
-		}
-		AnimationUtil.moveMarginAnimation(MarketDepthDetailView.this,
-				leftMargin, y);
-	}
+    private void moveView(int x, int y, int parentWidth) {
+        int leftMargin = 0;
+        if (x > parentWidth / 2) {
+            leftMargin = x - getWidth();
+        } else {
+            leftMargin = x;
+        }
+        AnimationUtil.moveMarginAnimation(MarketDepthDetailView.this,
+                leftMargin, y);
+    }
 
 }
