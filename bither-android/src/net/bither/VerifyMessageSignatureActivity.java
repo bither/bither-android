496970fb4bc95a7c665b0c0c90e5479bb56ee450
songchenwen <emptyzone.0@gmail.com>
2014-12-25 10:51:49 +0800

    edit verify message signature activity

--- bither-android/src/net/bither/VerifyMessageSignatureActivity.java
+++ bither-android/src/net/bither/VerifyMessageSignatureActivity.java
@@ -20,28 +20,34 @@ package net.bither;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import net.bither.bitherj.utils.PrivateKeyUtil;
 import net.bither.bitherj.utils.Utils;
 import net.bither.qrcode.ScanActivity;
 import net.bither.ui.base.DropdownMessage;
 import net.bither.ui.base.SwipeRightFragmentActivity;
-import net.bither.ui.base.dialog.DialogProgress;
 import net.bither.ui.base.listener.IBackClickListener;
 
 /**
  * Created by songchenwen on 14/12/19.
  */
 public class VerifyMessageSignatureActivity extends SwipeRightFragmentActivity {
-    private static final int AddressRequestCode = 1059, MessageRequestCode = 1100, SignatureRequestCode = 1101;
+    private static final int AddressRequestCode = 1059, MessageRequestCode = 1100,
+            SignatureRequestCode = 1101;
 
     private InputMethodManager imm;
     private EditText etAddress, etMessage, etSignature;
-    private Button btnQrAddrss, btnQrMessage, btnQrSignature, btnVerify;
+    private Button btnQrAddress, btnQrMessage, btnQrSignature, btnVerify;
+    private ImageView ivVerifySuccess, ivVerifyFailed;
+    private ProgressBar pb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,14 +63,20 @@ public class VerifyMessageSignatureActivity extends SwipeRightFragmentActivity {
         etAddress = (EditText) findViewById(R.id.et_address);
         etMessage = (EditText) findViewById(R.id.et_message);
         etSignature = (EditText) findViewById(R.id.et_signature);
-        btnQrAddrss = (Button) findViewById(R.id.btn_qr_address);
+        btnQrAddress = (Button) findViewById(R.id.btn_qr_address);
         btnQrMessage = (Button) findViewById(R.id.btn_qr_message);
         btnQrSignature = (Button) findViewById(R.id.btn_qr_signature);
         btnVerify = (Button) findViewById(R.id.btn_verify);
-        btnQrAddrss.setOnClickListener(qrClick);
+        pb = (ProgressBar) findViewById(R.id.pb_verify);
+        ivVerifyFailed = (ImageView) findViewById(R.id.iv_verify_failed);
+        ivVerifySuccess = (ImageView) findViewById(R.id.iv_verify_success);
+        btnQrAddress.setOnClickListener(qrClick);
         btnQrMessage.setOnClickListener(qrClick);
         btnQrSignature.setOnClickListener(qrClick);
         btnVerify.setOnClickListener(verifyClick);
+        etAddress.addTextChangedListener(textWatcher);
+        etMessage.addTextChangedListener(textWatcher);
+        etSignature.addTextChangedListener(textWatcher);
     }
 
     private View.OnClickListener verifyClick = new View.OnClickListener() {
@@ -79,10 +91,7 @@ public class VerifyMessageSignatureActivity extends SwipeRightFragmentActivity {
                 return;
             }
             imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-            final DialogProgress dp = new DialogProgress(VerifyMessageSignatureActivity.this,
-                    R.string.please_wait);
-            dp.setCanceledOnTouchOutside(false);
-            dp.show();
+            setVerifyButtonVerifying();
             new Thread() {
                 @Override
                 public void run() {
@@ -90,7 +99,11 @@ public class VerifyMessageSignatureActivity extends SwipeRightFragmentActivity {
                     btnVerify.post(new Runnable() {
                         @Override
                         public void run() {
-                            dp.dismiss();
+                            if(signed){
+                                setVerifyButtonSuccess();
+                            }else{
+                                setVerifyButtonFailed();
+                            }
                             DropdownMessage.showDropdownMessage(VerifyMessageSignatureActivity
                                             .this,
                                     signed ? R.string.verify_message_signature_verify_success : R
@@ -134,12 +147,15 @@ public class VerifyMessageSignatureActivity extends SwipeRightFragmentActivity {
             switch (requestCode) {
                 case AddressRequestCode:
                     etAddress.setText(input);
+                    resetVerifyButton();
                     break;
                 case MessageRequestCode:
                     etMessage.setText(input);
+                    resetVerifyButton();
                     break;
                 case SignatureRequestCode:
                     etSignature.setText(input);
+                    resetVerifyButton();
                     break;
                 default:
                     break;
@@ -148,6 +164,55 @@ public class VerifyMessageSignatureActivity extends SwipeRightFragmentActivity {
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+    private void resetVerifyButton() {
+        btnVerify.setEnabled(true);
+        btnVerify.setText(R.string.verify_message_signature_verify_button);
+        ivVerifySuccess.setVisibility(View.GONE);
+        ivVerifyFailed.setVisibility(View.GONE);
+        pb.setVisibility(View.GONE);
+    }
+
+    private void setVerifyButtonVerifying() {
+        btnVerify.setEnabled(false);
+        btnVerify.setText(R.string.verify_message_signature_verify_button_verifying);
+        ivVerifySuccess.setVisibility(View.GONE);
+        ivVerifyFailed.setVisibility(View.GONE);
+        pb.setVisibility(View.VISIBLE);
+    }
+
+    private void setVerifyButtonSuccess() {
+        btnVerify.setEnabled(true);
+        btnVerify.setText(R.string.verify_message_signature_verify_button_reverify);
+        ivVerifySuccess.setVisibility(View.VISIBLE);
+        ivVerifyFailed.setVisibility(View.GONE);
+        pb.setVisibility(View.GONE);
+    }
+
+    private void setVerifyButtonFailed() {
+        btnVerify.setEnabled(true);
+        btnVerify.setText(R.string.verify_message_signature_verify_button_reverify);
+        ivVerifySuccess.setVisibility(View.GONE);
+        ivVerifyFailed.setVisibility(View.VISIBLE);
+        pb.setVisibility(View.GONE);
+    }
+
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            resetVerifyButton();
+        }
+    };
 
     @Override
     public void finish() {
