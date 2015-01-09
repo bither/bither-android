36b04483e31e4cefddbe2269279592d7f27e438d
songchenwen <emptyzone.0@gmail.com>
2015-01-09 17:18:36 +0800

    edit hdm keychain add cold part

--- bither-android/src/net/bither/fragment/hot/AddAddressHotHDMFragment.java
+++ bither-android/src/net/bither/fragment/hot/AddAddressHotHDMFragment.java
@@ -149,7 +149,7 @@ public class AddAddressHotHDMFragment extends Fragment implements AddHotAddressA
                         }
                     });
                 }
-            });
+            }).show();
         }
     };
 
