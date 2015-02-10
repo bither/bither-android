59a65b3d2a324f95e39929679db609a60590822c
songchenwen <emptyzone.0@gmail.com>
2015-02-10 12:05:03 +0800

    add stop monitor to address option dialog

--- bither-android/src/net/bither/ui/base/dialog/DialogAddressWatchOnlyOption.java
+++ bither-android/src/net/bither/ui/base/dialog/DialogAddressWatchOnlyOption.java
@@ -17,14 +17,20 @@
 package net.bither.ui.base.dialog;
 
 import android.app.Activity;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import net.bither.BitherApplication;
 import net.bither.R;
+import net.bither.bitherj.api.http.BitherUrl;
 import net.bither.bitherj.core.Address;
 import net.bither.bitherj.utils.Utils;
-import net.bither.bitherj.api.http.BitherUrl;
+import net.bither.fragment.hot.HotAddressFragment;
+import net.bither.runnable.ThreadNeedService;
+import net.bither.service.BlockchainService;
+import net.bither.util.KeyUtil;
 import net.bither.util.UIUtil;
 
 import java.util.Locale;
@@ -53,6 +59,7 @@ public class DialogAddressWatchOnlyOption extends CenterDialog {
         ivBlockMeta = (ImageView) findViewById(R.id.iv_blockmeta);
         tvViewOnBlockchainInfo.setOnClickListener(viewOnBlockchainInfoClick);
         tvBlockMeta.setOnClickListener(viewOnBlockMetaClick);
+        findViewById(R.id.tv_delete).setOnClickListener(deleteClick);
         tvClose.setOnClickListener(closeClick);
         dialogQr = new DialogFancyQrCode(context, address.getAddress(), false, true);
         String defaultCountry = Locale.getDefault().getCountry();
@@ -89,6 +96,47 @@ public class DialogAddressWatchOnlyOption extends CenterDialog {
         }
     };
 
+    private View.OnClickListener deleteClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            new DialogConfirmTask(getContext(), getContext().getString(R.string
+                    .address_delete_confirmation), new Runnable() {
+                @Override
+                public void run() {
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            DialogProgress dp = new DialogProgress(activity, R.string.please_wait);
+                            dp.show();
+                            ThreadNeedService threadNeedService = new ThreadNeedService(dp,
+                                    activity) {
+                                @Override
+                                public void runWithService(BlockchainService service) {
+                                    KeyUtil.stopMonitor(service, address);
+                                    activity.runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            dp.dismiss();
+                                            afterDelete.run();
+                                            Fragment f = BitherApplication.hotActivity
+                                                    .getFragmentAtIndex(1);
+                                            if (f instanceof HotAddressFragment) {
+                                                HotAddressFragment hotAddressFragment =
+                                                        (HotAddressFragment) f;
+                                                hotAddressFragment.refresh();
+                                            }
+                                        }
+                                    });
+                                }
+                            };
+                            threadNeedService.start();
+                        }
+                    });
+                }
+            }).show();
+        }
+    };
 
     private View.OnClickListener closeClick = new View.OnClickListener() {
 
